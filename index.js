15da7b25b984d64c14ffad7f9542c939c1833ff7
DanielRendon1020 <d.danny1020@gmail.com>
2023-01-05 20:25:33 -0800

    turn text

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 
 // Select some elements...
+let header = document.querySelector('#page-header')
+header.style.transform = 'rotateX(180deg)'
+
+let images = document.querySelectorAll(' .dog-image')
+for(let i = 0; i < images.length; i++) {
+    images[i].style.filter = 'saturate(10000%) contrast(50%)'
+}
